00ea56e03af67d786cbbb653bf89af3446e59c41
github username <winter.lamon@gmail.com>
2018-01-03 10:35:47 -0500

    submitting code challenge

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,26 +1,38 @@
-import React, { Component } from 'react'
+import React from 'react'
 import TransactionsList from './TransactionsList'
 import CategorySelector from './CategorySelector'
-import {transactions} from '../transactionsData'
+// import {transactions} from '../transactionsData'
 
-class AccountContainer extends Component {
-  constructor() {
-    super()
-    //... your code here
+class AccountContainer extends React.Component {
+  state = {
+    transactions: [],
+    activeCategory: 'All'
   }
 
-  handleChange() {
-    //... your code here
+  componentDidMount = () => {
+    fetch('https://boiling-brook-94902.herokuapp.com/transactions')
+      .then(res => res.json())
+      .then(data => this.setState({transactions: data}))
+  }
+
+  handleChange(category) {
+    this.setState({activeCategory: category})
   }
 
   render() {
-    console.log(transactions)
+    console.log(this.state.activeCategory)
     return (
       <div className="ui grid container">
 
-        <CategorySelector />
+        <CategorySelector
+          activeCategory={this.state.activeCategory}
+          handleChange={this.handleChange}
+        />
 
-        <TransactionsList />
+        <TransactionsList
+          activeCategory={this.state.activeCategory}
+          transactions={this.state.transactions}
+        />
 
       </div>
     )
